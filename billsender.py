ea156af7a24453379f81b92fb6266dbafb22821d
kontained <kontained@gmail.com>
2015-06-23 18:35:40 -0500

    Added combobox
    Combobox allows the user to select which email account to send with.

--- billsender.py
+++ billsender.py
@@ -1,127 +1,134 @@
 from Tkinter import *
-from ttk import Button
+from ttk import Button, Combobox
 from PIL import Image, ImageTk
 from email.MIMEMultipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from time import localtime, strftime
-import os, glob, smtplib, win32com.client
+import smtplib
+import win32com.client
 
 class Window(Frame):
-		def __init__(self, parent):
-			Frame.__init__(self, parent, background = "grey")
-			self.parent = parent
-			self.centerWindow()
-			self.initUI()
+        def __init__(self, parent):
+            Frame.__init__(self, parent, background = "grey")
+            self.parent = parent
+            self.centerWindow()
+            self.initUI()
+            self.output = ""
 
-			filelist = glob.glob('*.pdf')
-			for file in filelist:
-				os.remove(file)
+        def initUI(self):
+            img = Image.open("welcomepage.jpg")
+            img = img.resize((250, 450), Image.ANTIALIAS)
+            WelcomeImage = ImageTk.PhotoImage(img)
 
-			filelist = glob.glob('*.jpg')
-			for file in filelist:
-				os.remove(file)
-				
-			self.output = ""
+            self.label1 = Label(self, image=WelcomeImage)
+            self.label1.image = WelcomeImage
+            self.label1.place (x = 10, y = 10)
+            
+            self.To = Entry(self)
+            self.To.place(x = 300, y = 10)
 
-		def initUI(self):
-			img = Image.open("welcomepage.png")
-			img = img.resize((250, 450), Image.ANTIALIAS)
-			WelcomeImage = ImageTk.PhotoImage(img)
+            self.box_value = StringVar()
+            self.box = Combobox(self, textvariable=self.box_value, state='readonly')
+            self.box['values'] = ('Dad', 'Mom')
+            self.box.current(0)
+            self.box.place(x = 300, y = 35)
 
-			self.label1 = Label(self, image=WelcomeImage)
-			self.label1.image = WelcomeImage
-			self.label1.place (x = 10, y = 10)
-			
-			self.To = Entry(self)
-			self.To.place(x = 300, y = 10)
+            lb = Listbox(self)
+            lb.place(x = 300, y = 65)
+            lb.bind("<<ListboxSelect>>", self.onSelect)
 
-			lb = Listbox(self)
-			lb.place(x = 300, y = 50)
-			lb.bind("<<ListboxSelect>>", self.onSelect)
+            with  open("recentEmails.txt", "r") as f:
+                for line in f:
+                    lb.insert(END, line)
 
-			with  open("recentEmails.txt", "r") as f:
-				for line in f:
-					lb.insert(END, line)
+            ScanButton = Button(self, text = "Scan Image", command = self.ScanClick)
+            ScanButton.place (x = 275, y = 250)
 
-			ScanButton = Button(self, text = "Scan Image", command = self.ScanClick)
-			ScanButton.place (x = 275, y = 250)
+            EmailButton = Button(self, text = "Email Bill", command = self.EmailClick)
+            EmailButton.place(x = 375, y = 250)
 
-			EmailButton = Button(self, text = "Email Bill", command = self.EmailClick)
-			EmailButton.place(x = 375, y = 250)
+        def centerWindow(self):
+            self.parent.title("Bill Sender")
+            self.pack(fill = BOTH, expand = 1)
+            w = 500
+            h = 500
 
-		def centerWindow(self):
-			self.parent.title("Bill Sender")
-			self.pack(fill = BOTH, expand = 1)
-			w = 500
-			h = 500
+            sw = self.parent.winfo_screenwidth()
+            sh = self.parent.winfo_screenheight()
 
-			sw = self.parent.winfo_screenwidth()
-			sh = self.parent.winfo_screenheight()
+            x = (sw - w) / 2
+            y = (sh - h) / 2
+            self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-			x = (sw - w) / 2
-			y = (sh - h) / 2
-			self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        def ScanClick(self):
+            wia = win32com.client.Dispatch("WIA.CommonDialog")
+            img = wia.ShowAcquireImage(0,0,65536,"{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}")
 
-		def ScanClick(self):
-			wia = win32com.client.Dispatch("WIA.CommonDialog")
-			img = wia.ShowAcquireImage(0,0,65536,"{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}")
+            fname = strftime("%I%M%S", localtime())
+            self.output = fname + ".pdf"
+            fname = fname + ".jpg"
+            img.SaveFile(fname)
 
-			fname = strftime("%I%M%S", localtime())
-			self.output = fname + ".pdf"
-			fname = fname + ".jpg"
-			img.SaveFile(fname)
+            ScanImg = Image.open(fname)
+            ScanImg = ScanImg.resize((250, 450), Image.ANTIALIAS)
+            ChangeImage = ImageTk.PhotoImage(ScanImg)
 
-			ScanImg = Image.open(fname)
-			ScanImg.save(self.output, "PDF", resolution = 100.0)
-			ScanImg = ScanImg.resize((250, 450), Image.ANTIALIAS)
-			ChangeImage = ImageTk.PhotoImage(ScanImg)
+            self.label1.configure(image = ChangeImage)
+            self.label1.image = ChangeImage
 
-			self.label1.configure(image = ChangeImage)
-			self.label1.image = ChangeImage
+            ScanImg.save(self.output, "PDF", resolution = 100.0)
 
-		def EmailClick(self):
-			From = 'jprucool@gmail.com'
-			To = self.To.get()
-			Sub = 'Traditional Mechanical Service Invoice'
-			Text = 'Please find invoice attached.'
-		
-			if not To:
-				return
+        def EmailClick(self):
+            if self.box.get() == 'Dad':
+                From = 'jprucool@gmail.com'
+                UserName = 'jprucool@gmail.com'
+                Password = 'pwd'
+                Sub = 'Traditional Mechanical Service Invoice'
+                Text = 'Please find invoice attached.'
+            else:
+                From = '4krealty@gmail.com'
+                UserName = '4krealty@gmail.com'
+                Password = 'pwd'
+                Sub = '4k Realty Documents'
+                Text = 'Please find attached files.'
+                            
+            To = self.To.get()
+            
+            if not To:
+                return
 
-			msg = MIMEMultipart()
-			msg['Subject'] = Sub
-			msg['From'] = From
-			msg['To'] = To
+            msg = MIMEMultipart()
+            msg['Subject'] = Sub
+            msg['From'] = From
+            msg['To'] = To
 
-			fp = open(self.output, 'rb')
-			pdf = MIMEApplication(fp.read(), _subtype="pdf")
-			fp.close()
-			pdf.add_header('Content-Disposition', 'attachment', filename = self.output)
+            fp = open(self.output, 'rb')
+            pdf = MIMEApplication(fp.read(), _subtype="pdf")
+            fp.close()
+            pdf.add_header('Content-Disposition', 'attachment', filename = self.output)
 
-			msg.attach(pdf)
-			
-			UserName = 'jprucool@gmail.com'
-			Password = 'pwd'
+            msg.attach(pdf)
 
-			Server = smtplib.SMTP('smtp.gmail.com:587')
-			Server.ehlo()
-			Server.starttls()
-			Server.login(UserName, Password)
-			Server.sendmail(From, To, msg.as_string())
-			Server.quit()
-			
-		def onSelect(self, val):
-			sender = val.widget
-			idx = sender.curselection()
-			value = sender.get(idx)
-			self.To.delete(0, END)
-			self.To.insert(0, value)
+            Server = smtplib.SMTP('smtp.gmail.com:587')
+            Server.ehlo()
+            Server.starttls()
+            Server.login(UserName, Password)
+            Server.sendmail(From, To, msg.as_string())
+            Server.quit()
+            
+        def onSelect(self, val):
+            sender = val.widget
+            idx = sender.curselection()
+            value = sender.get(idx)
+            self.To.delete(0, END)
+            self.To.insert(0, value)
 
 def main():
+
     root = Tk()
     root.resizable(0,0)
     app = Window(root)
     root.mainloop()
 
 if __name__ == '__main__':
-	main()
+    main()
